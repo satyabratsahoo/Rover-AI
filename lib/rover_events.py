2aa3603ef3270f4de374bed067c5c0f6fe58bcb3
Satyabrat <satyabratsahoo.kc@gmail.com>
2020-10-03 11:11:58 +0530

    Repo Setups

--- lib/rover_events.py
+++ lib/rover_events.py
@@ -1,6 +1,7 @@
 from lib.common import AppConfig
 import sys
 from lib.common import log
+from lib.voice import VoiceService
 # from lib.rover_navigation import Navigation
 
 
@@ -9,7 +10,7 @@ def process_event(arg_key):
     if arg_key != -1:
         char_value = chr(arg_key).upper() if arg_key != 32 else 'SPACE'
         if char_value in event_cfg:
-            log.info(event_cfg[char_value])
+            log.debug(f'Event Received: {event_cfg[char_value]}')
             if event_cfg[char_value] == 'exit':
                 sys.exit(0)
             # navigation = Navigation()
